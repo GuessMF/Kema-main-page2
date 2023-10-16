c1bb66824caea4b24125262fce787236ffd964aa
Sergey <Segas95@yandex.ru>
2023-10-16 14:51:23 +0300

    ff

--- scripts/script.js
+++ scripts/script.js
@@ -28,6 +28,27 @@ window.addEventListener("load", function () {
       ease: "power1.out",
     });
   }, 2000);
+
+  setTimeout(() => {
+    const moleculesImg = document.querySelector(".molecules-img");
+    gsap.fromTo(
+      moleculesImg,
+      {
+        scale: 1,
+      },
+      {
+        scale: 1.1,
+        duration: 0.5,
+        yoyo: true,
+        repeat: 1,
+        onComplete: function () {
+          moleculesImg.style.transform = "";
+          moleculesImg.style.transition = "transform 0.3s ease-in-out";
+        },
+      }
+    );
+  }, 2500);
+
   setTimeout(() => {
     const title = document.querySelector(".first-section__title");
     gsap.to(title, {
@@ -44,34 +65,58 @@ window.addEventListener("load", function () {
       ease: "power1.out",
     });
   }, 4000);
+  setTimeout(() => {
+    const onlineZalImg = document.querySelector(".online-zal-img");
+    gsap.fromTo(
+      onlineZalImg,
+      {
+        scale: 1,
+      },
+      {
+        scale: 1.1,
+        duration: 0.5,
+        yoyo: true,
+        repeat: 1,
+        onComplete: function () {
+          onlineZalImg.style.transform = "";
+          onlineZalImg.style.transition = "transform 0.3s ease-in-out";
+        },
+      }
+    );
+  }, 4500);
 });
 
-const moleculesImg = document.querySelector(".molecules-img");
-const onlineZalImg = document.querySelector(".online-zal-img");
-function animateMolecules() {
-  gsap.to(moleculesImg, {
-    scale: 1.1,
-    // rotationX: 30,
+const spans = document.querySelectorAll(".fourth-section__span");
+
+spans.forEach((span) => {
+  gsap.to(spans, {
+    x: () => gsap.utils.random(-15, 15),
+    y: () => gsap.utils.random(-15, 15),
     duration: 1,
+    repeat: -1,
     yoyo: true,
-    repeat: 1,
+  });
+});
+
+function getRandomSpan() {
+  const randomIndex = Math.floor(Math.random() * spans.length);
+  const randomSpan = spans[randomIndex];
+  gsap.to(randomSpan, {
+    opacity: 1,
+    color: "#ffffff",
+    textShadow: "0px 0px 5px rgba(249, 249, 249, 1)",
+    duration: 0.3,
     onComplete: function () {
-      animateMolecules();
+      setTimeout(() => {
+        gsap.to(randomSpan, {
+          opacity: 0.2,
+          color: "#000000",
+          textShadow: "none",
+          duration: 0.3,
+        });
+      }, Math.random() * 1000);
     },
   });
 }
 
-function animateZal() {
-  gsap.to(onlineZalImg, {
-    scale: 1.1,
-    // rotationX: 30,
-    duration: 1.1,
-    yoyo: true,
-    repeat: 1,
-    onComplete: function () {
-      animateZal();
-    },
-  });
-}
-animateMolecules();
-animateZal();
+setInterval(getRandomSpan, 1000);
